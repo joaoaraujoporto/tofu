2fb6529f3db37f1487663ce04bb1a62c699b9a1f
joao.porto <joaov.aporto@gmail.com>
2019-05-01 15:51:09 -0300

    Fixed absence of name in tokens

--- commons/src/main/java/io/tofu/commons/symbol/Token.java
+++ commons/src/main/java/io/tofu/commons/symbol/Token.java
@@ -1,21 +1,12 @@
 package io.tofu.commons.symbol;
 
-/*<<<<<<< HEAD
 public class Token<T> extends GSymbol<T> {
 	private String name;
 	
-	public Token(String name, T value) {
+	public Token(String name,T value) {
 		super(value);
-		this.terminal = true;
-=======*/
-public class Token<T> implements Terminal {
-	private String name;
-	private T value;
-	
-	public Token(String name, T value) {
 		this.name = name;
-		this.value = value;
-//>>>>>>> improv
+		this.terminal = true;
 	}
 	
 	@Override
@@ -26,8 +17,8 @@ public class Token<T> implements Terminal {
 	public String getName() {
 		return name;
 	}
-
-	public void setValue(T value) {
-		this.value = value;
+	
+	public T getValue() {
+		return value;
 	}
 }
