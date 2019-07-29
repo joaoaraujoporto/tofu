822df01cbd4fbf9fc4b606e1b7b118ec7c1e44cd
joao.porto <joaov.aporto@gmail.com>
2019-07-28 21:37:40 -0300

    Packages added

---
+++ tepas/src/main/java/io/tofu/tepas/procedures/StoreInTS.java
@@ -0,0 +1,15 @@
+package io.tofu.tepas.procedures;
+
+import io.tofu.commons.ts.TS;
+import io.tofu.commons.ts.TSEntry;
+import io.tofu.tepas.Procedure;
+
+public class StoreInTS extends Procedure {
+
+	@Override
+	public void apply() {
+		TS ts;
+		
+	}
+
+}
